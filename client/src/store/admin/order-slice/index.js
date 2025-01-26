ba28d24c27d94b77318aee29c0a15d76a574ee21
RoshanMhjn <162881378+RoshanMhjn@users.noreply.github.com>
2025-01-27 01:53:45 +0545

    add url

--- client/src/store/admin/order-slice/index.js
+++ client/src/store/admin/order-slice/index.js
@@ -10,7 +10,7 @@ export const getAllOrdersForAdmin = createAsyncThunk(
   "/order/getAllOrdersForAdmin",
   async () => {
     const response = await axios.get(
-      `http://localhost:5000/api/admin/orders/get`
+      `https://ecommerce-backend-6fmu.onrender.com/api/admin/orders/get`
     );
     return response.data;
   }
@@ -20,7 +20,7 @@ export const getOrderDetailsForAdmin = createAsyncThunk(
   "/order/getOrderDetailsForAdmin",
   async (id) => {
     const response = await axios.get(
-      `http://localhost:5000/api/admin/orders/details/${id}`
+      `https://ecommerce-backend-6fmu.onrender.com/api/admin/orders/details/${id}`
     );
     return response.data;
   }
@@ -30,7 +30,7 @@ export const updateOrderStatus = createAsyncThunk(
   "/order/updateOrderStatus",
   async ({ id, orderStatus }) => {
     const response = await axios.put(
-      `http://localhost:5000/api/admin/orders/update/${id}`,
+      `https://ecommerce-backend-6fmu.onrender.com/api/admin/orders/update/${id}`,
       {
         orderStatus,
       }
