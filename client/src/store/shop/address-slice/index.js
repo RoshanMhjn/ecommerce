18367d273c85156833b0fb38b873224034d9dcc3
RoshanMhjn <162881378+RoshanMhjn@users.noreply.github.com>
2025-01-27 01:55:59 +0545

    Update index.js

--- client/src/store/shop/address-slice/index.js
+++ client/src/store/shop/address-slice/index.js
@@ -11,7 +11,7 @@ export const addNewAddress = createAsyncThunk(
   "/addresses/addNewAddress",
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/shop/address/add",
+      "https://ecommerce-backend-6fmu.onrender.com/api/shop/address/add",
       formData
     );
     return response.data;
@@ -21,7 +21,7 @@ export const fetchAllAddresses = createAsyncThunk(
   "/addresses/fetchAllAddresses",
   async (userId) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/address/get/${userId}`
+      `https://ecommerce-backend-6fmu.onrender.com/api/shop/address/get/${userId}`
     );
     return response.data;
   }
@@ -30,7 +30,7 @@ export const editaAddress = createAsyncThunk(
   "/addresses/editaAddress",
   async ({ userId, addressId, formData }) => {
     const response = await axios.put(
-      `http://localhost:5000/api/shop/address/update/${userId}/${addressId}`,
+      `https://ecommerce-backend-6fmu.onrender.com/api/shop/address/update/${userId}/${addressId}`,
       formData
     );
     return response.data;
@@ -40,7 +40,7 @@ export const deleteAddress = createAsyncThunk(
   "/addresses/deleteAddress",
   async ({ userId, addressId }) => {
     const response = await axios.delete(
-      `http://localhost:5000/api/shop/address/delete/${userId}/${addressId}`
+      `https://ecommerce-backend-6fmu.onrender.com/api/shop/address/delete/${userId}/${addressId}`
     );
     return response.data;
   }
