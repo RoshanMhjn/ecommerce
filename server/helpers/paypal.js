4dcc586807c9c948dbf22675c5587b75470e16e2
RoshanMhjn <roshanmaharjan948@gmail.com>
2025-01-27 01:40:36 +0545

    complete

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,10 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "",
-  client_secret: "",
+  client_id:
+    "AeFTM4lx0dKtmChcQGlv796Dpov71mh1cEznbabbV9z7bGxQ_mPFPJcbr28I5ymPCd1aCY2pdIV_shDe",
+  client_secret:
+    "EPxRT3wy6l-EG_Vbzq7U9PYVliIhVYElVhSlvYAcQTzMMczNL8-o2W4vfmzjOUtRR9KbbyC7Q091b1Mw",
 });
 
 module.exports = paypal;
